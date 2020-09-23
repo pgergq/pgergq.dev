1d97830b94dfce9c6650100503d66182d13916da
pgergq <pgergq@gmail.com>
2020-09-23 16:27:18 +0200

    tweak About page styling

--- src/assets/jss/material-kit-react/views/aboutPage.jsx
+++ src/assets/jss/material-kit-react/views/aboutPage.jsx
@@ -1,11 +1,14 @@
-// TODO delete unneeded stuff
-
 import { container, title } from "assets/jss/material-kit-react.jsx"
-
 import imagesStyle from "assets/jss/material-kit-react/imagesStyles.jsx"
 
 const aboutPageStyle = {
-  container,
+  container: {
+    ...container,
+    zIndex: "2",
+    position: "relative",
+    paddingTop: "30vh",
+    color: "#FFFFFF",
+  },
   profile: {
     textAlign: "center",
     "& img": {
@@ -15,27 +18,20 @@ const aboutPageStyle = {
       transform: "translate3d(0, -50%, 0)",
     },
   },
-  description: {
-    margin: "1.071rem auto 0",
-    maxWidth: "600px",
-    color: "#999",
-    textAlign: "center !important",
+  cardHidden: {
+    opacity: "0",
+    transform: "translate3d(0, -60px, 0)",
+  },
+  cardFooter: {
+    paddingTop: "0rem",
+    border: "0",
+    borderRadius: "6px",
+    justifyContent: "center !important",
   },
   name: {
     marginTop: "-80px",
   },
   ...imagesStyle,
-  main: {
-    background: "#FFFFFF",
-    position: "relative",
-    zIndex: "3",
-  },
-  mainRaised: {
-    margin: "-60px 30px 0px",
-    borderRadius: "6px",
-    boxShadow:
-      "0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)",
-  },
   title: {
     ...title,
     display: "inline-block",
@@ -43,21 +39,6 @@ const aboutPageStyle = {
     marginTop: "30px",
     minHeight: "32px",
     textDecoration: "none",
-  },
-  socials: {
-    marginTop: "0",
-    width: "100%",
-    transform: "none",
-    left: "0",
-    top: "0",
-    height: "100%",
-    lineHeight: "41px",
-    fontSize: "20px",
-    color: "#999",
-  },
-  navWrapper: {
-    margin: "20px auto 50px auto",
-    textAlign: "center",
   },
   navLink: {
     color: "inherit",
@@ -74,6 +55,37 @@ const aboutPageStyle = {
     "&:hover,&:focus": {
       color: "inherit",
       background: "rgba(200, 200, 200, 0.2)",
+    },
+  },
+  pageHeader: {
+    minHeight: "100vh",
+    height: "auto",
+    display: "inherit",
+    position: "relative",
+    margin: "0",
+    padding: "0",
+    border: "0",
+    alignItems: "center",
+    "&:before": {
+      background: "rgba(0, 0, 0, 0.5)",
+    },
+    "&:before,&:after": {
+      position: "absolute",
+      zIndex: "1",
+      width: "100%",
+      height: "100%",
+      display: "block",
+      left: "0",
+      top: "0",
+      content: '""',
+    },
+    "& footer li a,& footer li a:hover,& footer li a:active": {
+      color: "#FFFFFF",
+    },
+    "& footer": {
+      position: "absolute",
+      bottom: "0",
+      width: "100%",
     },
   },
 }
--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,16 +1,37 @@
 import withStyles from "@material-ui/core/styles/withStyles"
 import profile from "assets/img/faces/gergely.jpeg"
+import image from "assets/img/profile-bg.jpg"
 import aboutPageStyle from "assets/jss/material-kit-react/views/aboutPage.jsx"
 import classNames from "classnames"
+import Card from "components/Card/Card.jsx"
+import CardBody from "components/Card/CardBody.jsx"
+import CardFooter from "components/Card/CardFooter.jsx"
 import Button from "components/CustomButtons/Button.jsx"
 import Footer from "components/Footer/Footer.jsx"
+import GridContainer from "components/Grid/GridContainer.jsx"
+import GridItem from "components/Grid/GridItem.jsx"
 import Header from "components/Header/Header.jsx"
 import HeaderLinks from "components/Header/HeaderLinks.jsx"
-import Parallax from "components/Parallax/Parallax.jsx"
-import React from "react"
+import { default as React } from "react"
 import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa"
 
 class AboutPage extends React.Component {
+  constructor(props) {
+    super(props)
+    // we use this to make the card to appear after the page has been rendered
+    this.state = {
+      cardAnimaton: "cardHidden",
+    }
+  }
+  componentDidMount() {
+    // we add a hidden class to the card and after 700 ms we delete it and the transition appears
+    setTimeout(
+      function () {
+        this.setState({ cardAnimaton: "" })
+      }.bind(this),
+      700
+    )
+  }
   render() {
     const { classes, ...rest } = this.props
     const imageClasses = classNames(
@@ -32,52 +53,67 @@ class AboutPage extends React.Component {
           }}
           {...rest}
         />
-        <Parallax small filter image={require("assets/img/profile-bg.jpg")} />
-        <div className={classNames(classes.main, classes.mainRaised)}>
-          <div>
-            <div className={classes.container}>
-              <div className={classes.profile}>
-                <div>
-                  <img src={profile} alt="me" className={imageClasses} />
-                </div>
-                <div className={classes.name}>
-                  <h3 className={classes.title}>gergely petrik</h3>
-                  <h6>software engineer</h6>
-                  <br />
-                </div>
-                <p>
-                  language-obsessed software developer from Hungary. <br />
-                  collector of LEGO&reg; Star Wars&trade; mini figures.
-                </p>
-                <br />
-                <Button
-                  color="transparent"
-                  href="https://github.com/pgergq"
-                  target="_blank"
-                  className={classes.navLink}
+        {/* <Parallax small filter image={require("assets/img/profile-bg.jpg")} /> */}
+        <div
+          className={classes.pageHeader}
+          style={{
+            backgroundImage: "url(" + image + ")",
+            backgroundSize: "cover",
+            backgroundPosition: "top center",
+          }}
+        >
+          <div className={classes.container}>
+            <GridContainer justify="center">
+              <GridItem xs={12} sm={12} md={6}>
+                <Card
+                  className={classNames(
+                    classes[this.state.cardAnimaton],
+                    classes.profile
+                  )}
                 >
-                  <FaGithub /> GitHub
-                </Button>
-                <Button
-                  color="transparent"
-                  href="https://www.linkedin.com/in/pgergq"
-                  target="_blank"
-                  className={classes.navLink}
-                >
-                  <FaLinkedin /> Linkedin
-                </Button>
-                <Button
-                  href="https://twitter.com/pgergq"
-                  target="_blank"
-                  color="transparent"
-                  className={classes.navLink}
-                >
-                  <FaTwitter /> Twitter
-                </Button>
-                <br />
-                <br />
-              </div>
-            </div>
+                  <div>
+                    <img src={profile} alt="me" className={imageClasses} />
+                  </div>
+                  <CardBody className={classes.profile}>
+                    <div className={classes.name}>
+                      <h3 className={classes.title}>gergely petrik</h3>
+                      <h6>software engineer</h6>
+                      <br />
+                    </div>
+                    <p>
+                      language-obsessed software developer from Hungary. <br />
+                      collector of LEGO&reg; Star Wars&trade; mini figures.
+                    </p>
+                  </CardBody>
+                  <CardFooter className={classes.cardFooter}>
+                    <Button
+                      color="transparent"
+                      href="https://github.com/pgergq"
+                      target="_blank"
+                      className={classes.navLink}
+                    >
+                      <FaGithub /> GitHub
+                    </Button>
+                    <Button
+                      color="transparent"
+                      href="https://www.linkedin.com/in/pgergq"
+                      target="_blank"
+                      className={classes.navLink}
+                    >
+                      <FaLinkedin /> Linkedin
+                    </Button>
+                    <Button
+                      href="https://twitter.com/pgergq"
+                      target="_blank"
+                      color="transparent"
+                      className={classes.navLink}
+                    >
+                      <FaTwitter /> Twitter
+                    </Button>
+                  </CardFooter>
+                </Card>
+              </GridItem>
+            </GridContainer>
           </div>
         </div>
         <Footer />
